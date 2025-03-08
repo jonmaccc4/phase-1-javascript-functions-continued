1d58176e7899260e69238a43a1babaa753013f57
jondev <jonmac2454@gmail.com>
2025-03-08 16:00:41 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return`This Saturday, I want to ${activity}!`; 
+} 
+console.log(saturdayFun());
+console.log(saturdayFun('go hiking'));
+
+function mondayWork (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork());
+console.log(mondayWork('work from home.'))
+
+
+function wrapAdjective(wrapper = '*') {
+    return function(adjective = 'special') {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
+
+
+const encouragingPromptFunction = wrapAdjective('||');
+console.log(encouragingPromptFunction('a dedicated programmer'));
+console.log(wrapAdjective()('a hard worker')); 
